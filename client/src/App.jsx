62cc4da217b88188ff69c24c08ab3ef22ce89b0b
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2023-06-05 23:32:50 +0545

    Route for Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import Register from "./Component/Register"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Newlogin from "./Component/Newlogin";
+import Navbar from "./Component/Navbar";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
+        {/* <Route path="/" element={<Navbar />} /> */}
         <Route path="/" element={<Newlogin />} />
-        <Route path="/signup" element={<Register/> } />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/home" element={<Navbar />} />
       </Routes>
     </BrowserRouter>
   )
