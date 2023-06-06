27a860ec0740b4f0ad32e50a5845405aac5bf6f7
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2023-06-06 22:17:02 +0545

    Modified route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import Register from "./Component/Register"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Newlogin from "./Component/Newlogin";
-import Navbar from "./Component/Navbar";
+import Home from "./Component/Homepage";
 
 function App() {
 
@@ -9,9 +9,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<Navbar />} /> */}
-        <Route path="/" element={<Newlogin />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Newlogin />} />
         <Route path="/signup" element={<Register />} />
-        <Route path="/home" element={<Navbar />} />
       </Routes>
     </BrowserRouter>
   )
